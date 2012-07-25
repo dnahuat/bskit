0d53f921a4d591df532693a67f016f2261223187
dnahuat <deiby.nahuat@gmail.com>
2012-07-25 18:31:48 -0500

    * Se adjunta licencia BSD * Se elimina BSWorker

--- src/main/java/com/baco/ui/containers/BSDlgThrowDetails.java
+++ src/main/java/com/baco/ui/containers/BSDlgThrowDetails.java
@@ -1,3 +1,33 @@
+/*
+ *   Copyright (c) 2012, Deiby Dathat Nahuat Uc
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met
+ *  1. Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *  3. All advertising materials mentioning features or use of this software
+ *  must display the following acknowledgement:
+ *  This product includes software developed by Deiby Dathat Nahuat.
+ *  4. Neither the name of Deiby Dathat Nahuat Uc nor the
+ *  names of its contributors may be used to endorse or promote products
+ *  derived from this software without specific prior written permission.
+
+ *  THIS SOFTWARE IS PROVIDED BY DEIBY DATHAT NAHUAT UC ''AS IS'' AND ANY
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL DEIBY DATHAT NAHUAT UC BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
 package com.baco.ui.containers;
 
 import java.io.Writer;
@@ -6,13 +36,13 @@ import java.util.ArrayList;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import javax.swing.KeyStroke;
-import com.baco.ui.core.BSWorker;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.util.concurrent.ExecutionException;
+import javax.swing.SwingWorker;
 
 /**
  * CHANGELOG
@@ -97,7 +127,7 @@ public class BSDlgThrowDetails extends BSDefaultDialog {
       }
    }
 
-   private class ShowDetailsWorker extends BSWorker<String, Void> {
+   private class ShowDetailsWorker extends SwingWorker<String, Void> {
 
       Throwable t;
 
--- src/main/java/com/baco/ui/containers/BSMainContainer.java
+++ src/main/java/com/baco/ui/containers/BSMainContainer.java
@@ -1,3 +1,33 @@
+/*
+ *   Copyright (c) 2012, Deiby Dathat Nahuat Uc
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met
+ *  1. Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *  3. All advertising materials mentioning features or use of this software
+ *  must display the following acknowledgement:
+ *  This product includes software developed by Deiby Dathat Nahuat.
+ *  4. Neither the name of Deiby Dathat Nahuat Uc nor the
+ *  names of its contributors may be used to endorse or promote products
+ *  derived from this software without specific prior written permission.
+
+ *  THIS SOFTWARE IS PROVIDED BY DEIBY DATHAT NAHUAT UC ''AS IS'' AND ANY
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL DEIBY DATHAT NAHUAT UC BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
 package com.baco.ui.containers;
 
 import java.awt.BorderLayout;
@@ -38,7 +68,6 @@ import com.baco.ui.core.BSCoreFactory;
 import com.baco.ui.core.BSElement;
 import com.baco.ui.core.BSOID;
 import com.baco.ui.core.BSSessionManager;
-import com.baco.ui.core.BSWorker;
 import com.baco.ui.menu.BSTreeNodeMenuItem;
 import com.baco.ui.renderers.BSMenuRenderer;
 import com.baco.ui.services.BSLogger;
@@ -53,6 +82,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import javax.swing.InputMap;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.SwingWorker;
 import javax.swing.UIManager;
 import javax.swing.plaf.TabbedPaneUI;
 import org.jdesktop.swingx.JXHyperlink;
@@ -476,7 +506,7 @@ public class BSMainContainer extends JXFrame implements BSCore {
       isFullscreen.set(fullscreen);
    }
 
-   private class DialogLoader extends BSWorker<BSCoreComponent, Void> {
+   private class DialogLoader extends SwingWorker<BSCoreComponent, Void> {
 
       private long loadOID;
       private final BSCoreComponent dialogForLoad;
@@ -1153,7 +1183,7 @@ public class BSMainContainer extends JXFrame implements BSCore {
       return success;
    }
 
-   private class TabLoader extends BSWorker<BSCoreComponent, Void> {
+   private class TabLoader extends SwingWorker<BSCoreComponent, Void> {
 
       private final BSCoreComponent itemForLoad;
       private long loadingOID;
